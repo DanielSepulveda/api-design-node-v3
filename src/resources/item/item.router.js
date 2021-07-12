51ea7968fce4da44c1d4ad6849bb715e29033b73
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-07-12 10:19:19 -0500

    lesosn 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,24 @@ import { Router } from 'express'
 
 const router = Router()
 
+router.get('/', (req, res) => {
+  return res.send('ok')
+})
+
+router.post('/', (req, res) => {
+  return res.send(req.body)
+})
+
+router.get('/:id', (req, res) => {
+  return res.send('ok')
+})
+
+router.put('/:id', (req, res) => {
+  return res.send(req.body)
+})
+
+router.delete('/:id', (req, res) => {
+  return res.send('ok')
+})
+
 export default router
